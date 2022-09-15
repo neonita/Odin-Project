736d471b2562c27104aa16dfbbdd4c5a25cd3278
Neonita <neonita888@gmail.com>
2022-09-15 19:20:04 -0400

    Create array of choices and use Math.floor() & Math.random() to randomly pick in array
    Create array of choices and use Math.floor() & Math.random() to randomly pick in array

--- javascript-foundations/rock-paper-scissor/script.js
+++ javascript-foundations/rock-paper-scissor/script.js
@@ -1,6 +1,15 @@
 // Randomly returns either 'rock', 'paper', or 'scissor'.
 function getComputerChoice() {
-  console.log("rock, paper, scissor");
+  let computerOptions = ["Rock", "Paper", "Scissor"];
+  let computerChoice = Math.floor(Math.random() * computerOptions.length);
+  return computerOptions[computerChoice];
 }
+
+// Plays a single round of Rock Paper Scissor.
+function play(playerSelection, computerSelection) {}
+
+// const playerSelection = String(
+//   prompt("Rock, Paper, or Scissors?")
+// ).toLowerCase();
 
 console.log(getComputerChoice());
