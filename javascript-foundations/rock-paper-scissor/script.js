c71df47352db3732eac52c0d235f820d31e3b0dd
Neonita <neonita888@gmail.com>
2022-09-15 19:48:13 -0400

    Create variables playerSelection and computerSelection
    playerSelection is assigned a prompt and computerSelection is assigned the getComputerChoice() function

--- javascript-foundations/rock-paper-scissor/script.js
+++ javascript-foundations/rock-paper-scissor/script.js
@@ -1,15 +1,20 @@
 // Randomly returns either 'rock', 'paper', or 'scissor'.
 function getComputerChoice() {
-  let computerOptions = ["Rock", "Paper", "Scissor"];
+  let computerOptions = ["rock", "paper", "scissor"];
   let computerChoice = Math.floor(Math.random() * computerOptions.length);
   return computerOptions[computerChoice];
 }
 
 // Plays a single round of Rock Paper Scissor.
-function play(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection, computerSelection);
+}
 
-// const playerSelection = String(
-//   prompt("Rock, Paper, or Scissors?")
-// ).toLowerCase();
+// Player's selection
+const playerSelection = String(
+  prompt("Rock, Paper, or Scissors?")
+).toLowerCase();
+// Computer's selection
+const computerSelection = getComputerChoice();
 
-console.log(getComputerChoice());
+console.log(playRound(playerSelection, computerSelection));
