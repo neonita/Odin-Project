8429edfd3d5241405a2e1e12d93cf66b33e975d8
Neonita <neonita888@gmail.com>
2022-09-15 20:03:44 -0400

    Set conditional for rock paper scissor game in playRound() function

--- javascript-foundations/rock-paper-scissor/script.js
+++ javascript-foundations/rock-paper-scissor/script.js
@@ -7,7 +7,29 @@ function getComputerChoice() {
 
 // Plays a single round of Rock Paper Scissor.
 function playRound(playerSelection, computerSelection) {
-  console.log(playerSelection, computerSelection);
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    console.log("It's a tie! 🗿🗿");
+  } else if (playerSelection === "rock" && computerSelection === "scissor") {
+    console.log("You Win! Rock beats scissor.");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log("You Lose! Scissor beats rock.");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log("You Win! Paper beats rock.");
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    console.log("It's a tie! 📃📃");
+  } else if (playerSelection === "paper" && computerSelection === "scissor") {
+    console.log("You Lose! Paper beats rock.");
+  } else if (playerSelection === "scissor" && computerSelection === "rock") {
+    console.log("You Lose! Rock beats scissor.");
+  } else if (playerSelection === "scissor" && computerSelection === "paper") {
+    console.log("You Win! Scissor beats paper.");
+  } else if (playerSelection === "scissor" && computerSelection === "scissor") {
+    console.log("It's a tie! ✂️✂️");
+  }
+}
+
+function game() {
+  playRound(playerSelection, computerSelection);
 }
 
 // Player's selection
